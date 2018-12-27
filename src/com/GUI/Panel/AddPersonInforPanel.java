383612fdc6c593b503d5d9f5acf1092dcc23d277
turingboot <528164177@qq.com>
2018-12-27 11:26:02 +0800

    11-27

--- src/com/GUI/Panel/AddPersonInforPanel.java
+++ src/com/GUI/Panel/AddPersonInforPanel.java
@@ -6,7 +6,22 @@ public class AddPersonInforPanel extends JPanel {
 
     public  static AddPersonInforPanel INSTANCE = new AddPersonInforPanel();
 
-    
+    public  JLabel nameLable = new JLabel("姓名");
+    public  JLabel emailLable = new JLabel("邮箱");
+    public  JLabel sexLable = new JLabel("性别");
+    public  JLabel jobLable = new JLabel("职业");
+
+    public JTextField  nameTextField = new JTextField();
+    public JTextField  emailTextField = new JTextField();
+    public JTextField  sexTextField = new JTextField();
+    public JTextField  jobTextField = new JTextField();
+
+    private AddPersonInforPanel(){
+        
+
+    }
+
+
 
 
 
